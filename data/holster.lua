bb4134d5d3c92d62f07efd91515f278650a08f95
CoreyJB247 <coreybruce1@protonmail.com>
2026-06-08 18:41:00 +0100

    Add files via upload

---
+++ data/holster.lua
@@ -0,0 +1,21 @@
+-- change holster clothing when pulling out or hosltering weapon.
+-- variation can be found here: https://docs.fivem.net/natives/?_0x67F3780DD425D4FC
+-- Male & female keys have these: {holstered, unholstered}
+
+return {
+    {
+        weapons = {`WEAPON_COMBATPISTOL`},
+        variation = 7,
+        male = {200, 204},
+    },
+    {
+        weapons = {`WEAPON_COMBATPISTOL`},
+        variation = 7,
+        male = {201, 205},
+    },
+    {
+        weapons = {`WEAPON_COMBATPISTOL`},
+        variation = 7,
+        male = {202, 206},
+    }
+}
